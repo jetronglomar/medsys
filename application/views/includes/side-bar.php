f54b070bc16b3d6b1fb7abb3b322fea072373958
Jet Ronrick Glomar <devngcp@gmail.com>
2019-05-13 12:15:17 +0800

    commit before creating initial table

---
+++ application/views/includes/side-bar.php
@@ -0,0 +1,77 @@
+<div class="col-md-3 left_col">
+          <div class="left_col scroll-view">
+            <div class="navbar nav_title" style="border: 0;">
+              <a href="index.html" class="site_title"><i class="fa fa-stethoscope"></i> <span>JG | MedSys</span></a>
+            </div>
+
+            <div class="clearfix"></div>
+
+            <!-- menu profile quick info -->
+            <div class="profile clearfix">
+              <div class="profile_pic">
+                <img src="<?php echo base_url();?>resources/images/img.jpg" alt="..." class="img-circle profile_img">
+              </div>
+              <div class="profile_info">
+                <span>Welcome,</span>
+                <h2>John Doe</h2>
+              </div>
+            </div>
+            <!-- /menu profile quick info -->
+
+            <br />
+
+            <!-- sidebar menu -->
+            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+              <div class="menu_section">
+                <h3>General</h3>
+                <ul class="nav side-menu">
+                  <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="index.html">Dashboard</a></li>
+                      <li><a href="index2.html">Profile</a></li>
+                    </ul>
+                  </li>
+                
+                  <li><a><i class="fa fa-group"></i> Patient Management <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="index.html">Profile</a></li>
+                      <li><a href="index2.html">Laboratory Result</a></li>
+                      <li><a href="index3.html">Schedule</a></li>
+                      <li><a href="index3.html">Activities</a></li>
+                    </ul>
+                  </li>
+
+                  
+                  <li><a><i class="fa fa-desktop"></i> Queries <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="general_elements.html">Patient History</a></li>
+                    </ul>
+                  </li>
+                  <li><a><i class="fa fa-table"></i> Reports <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="tables.html">Patient History</a></li>
+                      <li><a href="tables_dynamic.html">TBA</a></li>
+                    </ul>
+                  </li>
+
+                  <li><a><i class="fa fa-cogs"></i> Configuration <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="form.html">Doctor</a></li>
+                      <li><a href="form_advanced.html">Nurse</a></li>
+                      <li><a href="form_validation.html">Specialization</a></li>
+                      <li><a href="form_wizards.html">Patient</a></li>
+                      <li><a href="form_upload.html">User Account</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+              
+
+            </div>
+            <!-- /sidebar menu -->
+
+            <!-- /menu footer buttons -->
+       
+            <!-- /menu footer buttons -->
+          </div>
+        </div>
